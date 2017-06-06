9315703432faa5f9b5c2864728cd97730b31e684
Julien Chavanton <jchavanton@gmail.com>
2017-06-06 21:12:02 +0000

    multi process

--- db_action.c
+++ db_action.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sqlite3.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -26,7 +27,7 @@ void db_query (sqlite3 *conn, const char *query ) {
 
 	int rc = sqlite3_prepare_v2(conn, query, strlen(query), &stmt, NULL);
 	if (rc != SQLITE_OK) {
-		printf("error prepare query [%s]\n", sqlite3_errmsg(conn));	
+		printf("error prepare query [%s]\n", sqlite3_errmsg(conn));
 		// error prepare query [database is locked]
 		return;
 	}	
@@ -43,7 +44,7 @@ void db_query (sqlite3 *conn, const char *query ) {
 			int c=0;
 			printf("sqlite3_column_count: %d\n", rc);
 			// sqlite3_column_name(stmt, c);
-			// sqlite3_column_decltype(stmt, c);	
+			// sqlite3_column_decltype(stmt, c);
 			// sqlite3_column_type(stmt, c)
 		}
 	}
@@ -56,7 +57,7 @@ void db_query (sqlite3 *conn, const char *query ) {
 	}
 }
 
-int main () {
+int work(int pid) {
 	const char *db = "/var/lib/docker/volumes/neustardb/_data/neustar.sqlite";
 	sqlite3 *conn = db_connect(db);
 	char query[1024];
@@ -65,9 +66,7 @@ int main () {
 	char ts[26];
 	struct tm* tm_info;
 	struct timeval stop, start;
-
-//do stuff
-
+	printf("worker started[%d]\n", pid);
 	while (1) {
 		// int n = rand() % 8;
 		// 2012000014 key
@@ -77,20 +76,36 @@ int main () {
 
 		char num[10];
 		snprintf(num,11,"%d%d%d%d%d%d%d%d%d%d",rand()%8+1,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9);
-		snprintf(query,1024,"SELECT cast(lrn as text) as lrn, spid, 1 as ord FROM sv WHERE key = %.10s"          
+		snprintf(query,1024,"SELECT cast(lrn as text) as lrn, spid, 1 as ord FROM sv WHERE key = %.10s"
 	" UNION SELECT cast(lrn as text) as lrn, spid, 2 as ord FROM block   WHERE key = %.10s"
 	" UNION SELECT cast(lrn as text) as lrn, spid, 3 as ord FROM npanxxx WHERE key = %.7s"
 	" UNION SELECT cast(lrn as text) as lrn, spid, 4 as ord FROM npanxx  WHERE key = %.6s"
-	" ORDER BY ord ASC LIMIT 1", num, num, num, num	); 
+	" ORDER BY ord ASC LIMIT 1", num, num, num, num);
 
 		// snprintf(query,1024,"select * from sv where key = %s limit 1", num);
 		// snprintf(query,1024,"select * from sv limit 1");
 		gettimeofday(&start, NULL);
 		db_query(conn, query);
 		gettimeofday(&stop, NULL);
-		//printf("done\n");
-		printf("[%s][%d][%s]\n",ts, (int)(stop.tv_usec - start.tv_usec) ,query);
-		//usleep(100000);
-		// usleep(1000);
+		int sleep_us = 100 * (rand()%1000);
+		printf("[%d][%d][%s][%d][%s]\n",pid,sleep_us,ts,(int)(stop.tv_usec-start.tv_usec),query);
+		// usleep(100000);
+		usleep(sleep_us);
 	}
 }
+
+int main() {
+	pid_t pid;
+	int p;
+	printf("main\n");
+	int max_process = 4;
+	for (p=0;p<max_process;p++) {
+		pid = fork();
+		if (pid == 0) {
+			printf("child pid[%d][%d]\n",pid,p);
+			work(getpid());
+		}
+	}
+	work(pid);
+}
+
