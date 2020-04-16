b8d90aab6d2f51b07fa9979e0265fdbfbc58d198
Julien Chavanton <jchavanton@preprod-ro-us-west-or-01.i.prod.q.fl.gg>
2020-04-16 23:19:59 +0000

    fix forking and randomizer

--- src/db_action.c
+++ src/db_action.c
@@ -4,10 +4,20 @@
 #include <sqlite3.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdint.h>
 
+static int debug;
 
+long time_diff(struct timeval x , struct timeval y) {
+	long x_ms , y_ms , diff;
+	x_ms = (long)x.tv_sec*1000000 + (long)x.tv_usec;
+	y_ms = (long)y.tv_sec*1000000 + (long)y.tv_usec;
+	diff = (long)y_ms - (long)x_ms;
+	return diff;
+}
 
 void db_query (sqlite3 *conn, const char *query ) {
 	int i;
@@ -30,7 +40,8 @@ void db_query (sqlite3 *conn, const char *query ) {
 			/* get column types */
 			rc = sqlite3_column_count(stmt);
 			int c=0;
-			printf("sqlite3_column_count: %d\n", rc);
+			if (debug)
+				printf("sqlite3_column_count: %d\n", rc);
 			// sqlite3_column_name(stmt, c);
 			// sqlite3_column_decltype(stmt, c);
 			// sqlite3_column_type(stmt, c)
@@ -63,28 +74,30 @@ sqlite3 * db_connect (const char *db_name) {
 }
 
 int work(int pid) {
-	const char *db = "/var/lib/docker/volumes/neustardb/_data/neustar.sqlite";
+	const char *db = "/opt/flowroute/neustardb/neustar.sqlite";
 	char query[1024];
 	sqlite3 *conn = db_connect(db);
 	if (!conn) {
 		printf("error connecting to [%s]\n", db);
 		return -1;
 	}
-	srand(time(NULL));
+
 	time_t timer;
 	char ts[26];
 	struct tm* tm_info;
 	struct timeval stop, start;
 	printf("worker started[%d]\n", pid);
 	int count=0;
-	while (1) {
+	long max_us=0;
+
+	char num[10];
+	while (count < 10000) {
 		// int n = rand() % 8;
 		// 2012000014 key
 		time(&timer);
 		tm_info = localtime(&timer);
 		strftime(ts, 26, "%Y-%m-%d %H:%M:%S", tm_info);
 
-		char num[10];
 		snprintf(num,11,"%d%d%d%d%d%d%d%d%d%d",rand()%8+1,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9);
 		snprintf(query,1024,"SELECT cast(lrn as text) as lrn, spid, 1 as ord FROM sv WHERE key = %.10s"
 	" UNION SELECT cast(lrn as text) as lrn, spid, 2 as ord FROM block   WHERE key = %.10s"
@@ -97,27 +110,45 @@ int work(int pid) {
 		gettimeofday(&start, NULL);
 		db_query(conn, query);
 		gettimeofday(&stop, NULL);
-		int sleep_us = 100 * (rand()%1000);
-		printf("[%d][%d][%s][%d][%s]\n",pid,sleep_us,ts,(int)(stop.tv_usec-start.tv_usec),query);
-		// usleep(100000);
-		usleep(sleep_us);
+		uint32_t sleep_us = 100 * (rand()%1000);
+		// printf("pid[%d]count[%d][%d][%s][%d][%s]\n",pid,count,sleep_us,ts,(int)(stop.tv_usec-start.tv_usec),query);
+		// uint32_t elapsed_us = stop.tv_usec-start.tv_usec;
+		long elapsed_us  = time_diff(start, stop);
+		if (max_us < elapsed_us) max_us = elapsed_us;
+		if (debug)
+			printf("pid[%d]count[%d]number[%s][%ldus]\n",pid,count,num,elapsed_us);
+
+		// usleep(1000000);
+		//usleep(sleep_us);
 		count++;
 	}
-	return count;
+	printf("pid[%d] done[%d] max_ms[%dms]\n", pid, count, (uint32_t)(max_us/1024));
+	fflush(stdout);
+	exit(count);
 }
 
 int main() {
-	pid_t pid;
+	int max_process = 10;
+	pid_t pid[max_process];
 	int p;
-	printf("main\n");
-	int max_process = 4;
+	struct timeval stop, start;
+	printf(">>>> starting workers: %d\n", max_process);
+
+	gettimeofday(&start, NULL);
+	
 	for (p=0;p<max_process;p++) {
-		pid = fork();
-		if (pid == 0) {
-			printf("child pid[%d][%d]\n",pid,p);
+		pid[p] = fork();
+		srand(time(NULL) ^ (getpid()<<16));
+		if (pid[p] == 0) {
+			printf("child pid[%d][%d]\n", pid[p], p);
 			work(getpid());
 		}
 	}
-	work(pid);
+	for (p=0;p<max_process;p++) {
+		waitpid(pid[p], NULL, 0);
+	}
+	gettimeofday(&stop, NULL);
+	long elapsed_us  = time_diff(start, stop);
+	printf(">>> done, elapsed_ms[%d]\n", (uint32_t)(elapsed_us/1024));
 }
 
