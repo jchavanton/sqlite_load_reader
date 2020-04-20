953fb5f094e8ef90f7829bfaac9431bd30f1e112
Julien Chavanton <jchavanton@gmail.com>
2020-04-20 22:52:58 +0000

    saving

--- src/db_action.c
+++ src/db_action.c
@@ -92,22 +92,29 @@ sqlite3 * db_connect (const char *db_name) {
 	// PRAGMA journal_mode=WAL;
 	db_query(conn, "PRAGMA journal_mode=WAL", 1);
 	db_query(conn, "PRAGMA CACHE_SIZE", 1);
-//	db_query(conn, "PRAGMA synchronous=NORMAL", 1);
+	//db_query(conn, "PRAGMA synchronous=NORMAL", 1);
 	db_query(conn, "PRAGMA synchronous", 1);
 	return conn;
 }
 
 typedef struct stats {
-        int max_us;
-        float average;  // weigthed average, estimate of the last few weeks
-        int32_t count;
+        int max_us;     // max explased time (slowest query)
+	int max_idx;    // index of the slowest query
+	int slow;       // slow queries
+	char num[10];
+	float average;  // average query time
+	int32_t count;
 } stats_t;
 
-void update_stats(stats_t* stats, int elapsed_us) {
+void update_stats(stats_t* stats, int elapsed_us, char *num) {
 	stats->count++;
 	if (stats->max_us < elapsed_us) {
 		stats->max_us = elapsed_us;
+		stats->max_idx = stats->count;
+		strcpy(stats->num, num);
 	}
+	if (elapsed_us >= 100000)
+		stats->slow++;
 	float delta = elapsed_us - stats->average;
 	stats->average += delta/stats->count;
 }
@@ -141,11 +148,12 @@ int work(int pid) {
 
 		snprintf(num,11,"%d%d%d%d%d%d%d%d%d%d",rand()%8+1,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9,rand()%9);
 		snprintf(query,1024,"SELECT cast(lrn as text) as lrn, spid, 1 as ord FROM sv WHERE key = %.10s"
-	" UNION SELECT cast(lrn as text) as lrn, spid, 2 as ord FROM block   WHERE key = %.10s"
-	" UNION SELECT cast(lrn as text) as lrn, spid, 3 as ord FROM npanxxx WHERE key = %.7s"
-	" UNION SELECT cast(lrn as text) as lrn, spid, 4 as ord FROM npanxx  WHERE key = %.6s"
-	" ORDER BY ord ASC LIMIT 1", num, num, num, num);
+		" UNION SELECT cast(lrn as text) as lrn, spid, 2 as ord FROM block   WHERE key = %.10s"
+		" UNION SELECT cast(lrn as text) as lrn, spid, 3 as ord FROM npanxxx WHERE key = %.7s"
+		" UNION SELECT cast(lrn as text) as lrn, spid, 4 as ord FROM npanxx  WHERE key = %.6s"
+		" ORDER BY ord ASC LIMIT 1", num, num, num, num);
 
+		// printf("\n%s\n", query);
 		// snprintf(query,1024,"select * from sv where key = %s limit 1", num);
 		// snprintf(query,1024,"select * from sv limit 1");
 		gettimeofday(&start, NULL);
@@ -155,7 +163,7 @@ int work(int pid) {
 		// printf("pid[%d]count[%d][%d][%s][%d][%s]\n",pid,stats.count,sleep_us,ts,(int)(stop.tv_usec-start.tv_usec),query);
 		// uint32_t elapsed_us = stop.tv_usec-start.tv_usec;
 		long elapsed_us  = time_diff(start, stop);
-		update_stats(&stats, elapsed_us);
+		update_stats(&stats, elapsed_us, num);
 
 		if (debug)
 			printf("pid[%d]count[%d]number[%s][%ldus]\n",pid,stats.count,num,elapsed_us);
@@ -164,8 +172,9 @@ int work(int pid) {
 		// usleep(sleep_us);
 	}
 	kbytes = get_mem_usage();
-	printf("pid[%d]done[%d]max_ms[%dms]avg_ms[%dus]mem[%"PRIu32"KB/%"PRIu32"MB]\n",
-		pid, stats.count, (uint32_t)(stats.max_us/1000), (uint32_t)(stats.average), kbytes, kbytes/1024);
+	// printf("%s", query);
+	printf("pid[%d]done[%d]slow[%d]max_ms[%dms][%d|%s]avg_ms[%dus]mem[%"PRIu32"KB/%"PRIu32"MB]\n",
+		pid,stats.count,stats.slow,(uint32_t)(stats.max_us/1000),stats.max_idx,stats.num,(uint32_t)(stats.average), kbytes, kbytes/1024);
 	fflush(stdout);
 	exit(stats.count);
 }
